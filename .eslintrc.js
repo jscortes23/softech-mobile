857b37062b07c239a2abb57e54b57a9dcc228e57
jscortes23 <js.199911@gmail.com>
2024-04-26 20:36:20 -0500

    Add ESlint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'universe',
+    'universe/shared/typescript-analysis',
+  ],
+  overrides: [
+    {
+      files: [
+        '*.ts',
+        '*.tsx',
+        '*.d.ts'
+      ],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+    },
+  ],
+};
