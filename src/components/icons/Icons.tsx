6c57cb98d2853b055252b65260750e4e725fc4ac
Mikey-V <mv.miguel@gmail.com>
2024-05-02 08:59:34 -0500

    Crear los iconos como componentes

---
+++ src/components/icons/Icons.tsx
@@ -0,0 +1,318 @@
+import { Image } from "react-native"
+interface iconsProps {
+  width: number
+  height: number
+  color?: string
+}
+
+export const ActivityIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Activity.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ArrowDownIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Arrow-Down.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ArrowLeftIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Arrow-Left.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ArrowRghtIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Arrow-Right.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ArrowUpIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Arrow-Up.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const BellIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Bell.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ChevronDownIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Chevron-Down.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ChevronLeftIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Chevron-Left.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ChevronRightIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Chevron-Right.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ChevronUpIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Chevron-Up.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const EditIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Edit.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ExternalLinkIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/External-Link.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const GridIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Grid.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const MailIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Mail.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const MembersIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Members.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const MenuIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Menu.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const MinusIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Minus.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const MoreVerticalIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/More-Vertical.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const PageIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Page.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const PlusIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Plus.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const ProductIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Product.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const Refreshcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Refresh.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const SearchIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Search.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const SettingsIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Settings.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const SiteIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Site.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
+
+export const TagIcon: React.FC<iconsProps> = (props) => {
+  const { width, height, color } = props
+  return (
+    <Image source={require('../../assets/icons/Tag.png')}
+      style={{
+        width: width,
+        height: height,
+        tintColor: color
+      }} />
+  )
+}
