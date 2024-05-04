7bd8c7b0375ca3eead582a5a7bbe23bd579612c4
Mikey-V <mv.miguel@gmail.com>
2024-05-04 17:23:40 -0500

    Crear componente para degradados lineares

---
+++ src/components/backgrounds/BgLinear.tsx
@@ -0,0 +1,17 @@
+import { LinearGradient } from 'expo-linear-gradient'
+
+interface BgLinearProps {
+  color: string[]
+  start: { x: number; y: number }
+  end: { x: number; y: number }
+  children?: React.ReactNode
+}
+
+export const BgLinear: React.FC<BgLinearProps> = (props) => {
+  const { color, start, end, children } = props
+  return (
+    <LinearGradient colors={color} start={start} end={end}>
+      {children}
+    </LinearGradient>
+  )
+}
