44277cafce86180a552d7f98404dfdae16ddf154
Mikey-V <mv.miguel@gmail.com>
2024-05-09 20:04:47 -0500

    Crear component ProductList

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, View } from 'react-native'
+
+import { StyledText } from '../StyledText'
+import { SearchIcon } from '../icons/Icons'
+
+interface ProductListProps {
+  name: string
+  children: React.ReactNode
+}
+
+export const ProductList: React.FC<ProductListProps> = (props) => {
+  const { name, children } = props
+  return (
+    <>
+      <View style={styles.header}>
+        <StyledText subtitle1 neutral100>
+          {name}
+        </StyledText>
+        <SearchIcon height={30} width={30} />
+      </View>
+      <View style={styles.body}>{children}</View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    columnGap: 230,
+    paddingBottom: 5,
+    flexDirection: 'row',
+  },
+  body: {
+    flexDirection: 'column',
+  },
+})
