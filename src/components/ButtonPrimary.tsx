20cce344a9423869be74408b429e6e0bc97e0423
Rodrigo Morales Varela <moralesvarelarodrigo0@gmail.com>
2024-05-30 20:46:50 -0500

    Añadir una nueva variante al ButtonPrimary

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -7,46 +7,56 @@ interface ButtonPrimaryProps {
   onPress?: () => void
   text?: string
   icon?: React.ReactNode
-  variant: 'primary' | 'secondary'
+  variant: 'primary' | 'secondary' | 'alternative'
 }
 
 export const ButtonPrimary: React.FC<ButtonPrimaryProps> = (props) => {
   const { icon, onPress, text, variant } = props
 
-  const type = variant === 'primary' ? styles.primary : styles.secondary
+  const type =
+    variant === 'primary'
+      ? styles.primary
+      : variant === 'secondary'
+        ? styles.secondary
+        : styles.alternative
 
   return (
     <Pressable onPress={onPress} style={[styles.container, styles.shadowProp, type]}>
       {icon}
-      {text && <StyledText style={type}>{text}</StyledText>}
+      {text && <StyledText style={{ color: type.color }}>{text}</StyledText>}
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 10,
-    paddingVertical: 15,
-    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 10,
     columnGap: 10,
+    flexDirection: 'row',
+    fontSize: fontSize.body1,
+    justifyContent: 'center',
+    paddingVertical: 15,
   },
   shadowProp: {
+    elevation: 5,
     shadowColor: "rgba('51','102','204','0.2')",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
-    elevation: 5,
   },
   primary: {
     backgroundColor: colors.blueBase,
     color: colors.white,
-    fontSize: fontSize.subtitle1,
   },
   secondary: {
     backgroundColor: colors.brand10,
     color: colors.black,
-    fontSize: fontSize.subtitle2,
+  },
+  alternative: {
+    backgroundColor: colors.white,
+    borderColor: colors.blueBase,
+    borderWidth: 1,
+    color: colors.blueBase,
   },
 })
