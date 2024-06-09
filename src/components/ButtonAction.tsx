97e200ad64e67991e8f7d724776021976bcc3330
AndresSCC <132695823+AndresSCC@users.noreply.github.com>
2024-06-09 16:11:15 -0500

    Crear componente ButtonAction

--- src/components/ButtonAction.tsx
+++ src/components/ButtonAction.tsx
@@ -1,38 +1,36 @@
-import { StyleSheet, Pressable, Text, ViewStyle } from 'react-native'
+import { StyleSheet, Pressable } from 'react-native'
 
-import { ChevronDownIcon } from './icons/Icons'
+import { StyledText } from './StyledText'
 import { colors } from '../config/themes/appThemes'
 
 interface ButtonActionProps {
   text: string
   icon?: React.ReactNode
-  style?: ViewStyle
+  onPress?: () => void
 }
 
 export const ButtonAction: React.FC<ButtonActionProps> = (props) => {
-  const { text, icon, style } = props
+  const { text, icon, onPress } = props
 
   return (
-    <Pressable style={[styles.button, style]}>
-      <Text style={styles.text}>{text}</Text>
-      {icon ?? <ChevronDownIcon width={24} height={24} color={colors.black} />}
+    <Pressable style={styles.container} onPress={onPress}>
+      <StyledText body1 black>
+        {text}
+      </StyledText>
+      {icon}
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingVertical: 10,
-    paddingHorizontal: 16,
+    padding: 16,
     borderWidth: 1,
-    borderColor: colors.neutralBase,
+    borderColor: colors.neutral40,
     borderRadius: 8,
-  },
-  text: {
-    fontSize: 16,
-    color: colors.black,
   },
 })
