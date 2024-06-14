d18b2b45b8eaca1b3a9f6fa6d14b23b6599bab10
jscortes23 <js.199911@gmail.com>
2024-06-14 09:38:53 -0500

    Dejar el drawerNavigation como principal

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 
-import { ImageGallery } from './components/ImageGallery'
 import { DrawerNavigation } from './navigators/DrawerNavigation'
 
 export default function Main() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <ImageGallery />
+      <DrawerNavigation />
     </View>
   )
 }
