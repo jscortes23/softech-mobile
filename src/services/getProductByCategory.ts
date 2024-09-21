bf65d9143956b9dcea57503705277c4e7337a518
Milton Yumbo <pachastriker@gmail.com>
2024-09-20 22:38:11 -0500

    Utilizar endpoint para llamar hardware de la API

---
+++ src/services/getProductByCategory.ts
@@ -0,0 +1,13 @@
+export const getProductsByCategory = async (id_categoria: number) => {
+  try {
+    const res = await fetch(`http://192.168.1.60:8000/api/productos/categoria/${id_categoria}`)
+    if (!res.ok) {
+      throw new Error('Error fetching products')
+    }
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching products by category:', error)
+    throw error
+  }
+}
